85d146f04de25cc0d7a8051504034a55f630d204
Larry Devlin <larrydevlin1770@gmx.com>
2021-04-25 21:21:58 -0400

    New URL, view and template to display lists

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -92,7 +92,7 @@ class NewVisitorTest(LiveServerTestCase):
 
         # Francis visits the home page. There is no sign of Edith's
         # list.
-        self.browser.get(self.liver_server_url)
+        self.browser.get(self.live_server_url)
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Buy peacock feathers', page_text)
         self.assertNotIn('make a fly', page_text)
@@ -114,5 +114,4 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertNotIn('Buy peacock feathers', page_text)
         self.assertIn('Buy milk', page_text)
 
-        # Satisfied, they both go back to sleep.
-        
+        # Satisfied, they both go back to sleep.  
